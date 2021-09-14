875a8c900168420175f1e5750e6607107fa6d89d
rajiniw95 <rajiniw95@gmail.com>
2021-09-14 10:28:25 -0500

    revert previous commit

--- courses/src/main/java/com/coursemgmt/courses/controller/CourseController.java
+++ courses/src/main/java/com/coursemgmt/courses/controller/CourseController.java
@@ -20,13 +20,13 @@ public class CourseController {
         return "index";
     }
 
-//    // Rest controller for viewHomePage method -- duplicate method with @ResponseBody annotation
-//    @GetMapping("/api")
-//    @ResponseBody
-//    public String viewHomePageRestAPI(Model model){
-//        model.addAttribute("listCourses", courseService.getAllCourses());
-//        return "index";
-//    }
+    // Rest controller for viewHomePage method -- duplicate method with @ResponseBody annotation
+    @GetMapping("/api")
+    @ResponseBody
+    public String viewHomePageRestAPI(Model model){
+        model.addAttribute("listCourses", courseService.getAllCourses());
+        return "index";
+    }
 
     @GetMapping("/showNewCourseForm")
     public String showNewCourseForm(Model model){
@@ -43,14 +43,14 @@ public class CourseController {
         return "redirect:/";
     }
 
-//    // Rest controller for showNewCourseForm method -- duplicate method with @ResponseBody annotation
-//    @PostMapping("/saveCourse/api")
-//    @ResponseBody
-//    public String saveCourseRestAPI(@ModelAttribute("course") Course course){
-//        // save course to database
-//        courseService.saveCourse(course);
-//        return "redirect:/";
-//    }
+    // Rest controller for showNewCourseForm method -- duplicate method with @ResponseBody annotation
+    @PostMapping("/saveCourse/api")
+    @ResponseBody
+    public String saveCourseRestAPI(@ModelAttribute("course") Course course){
+        // save course to database
+        courseService.saveCourse(course);
+        return "redirect:/";
+    }
 
     @GetMapping("/showFormForUpdate/{id}")
     public String showFormForUpdate(@PathVariable( value = "id") long id, Model model) {
@@ -63,18 +63,18 @@ public class CourseController {
         return "update_course";
     }
 
-//    // Rest controller for showFormForUpdate method -- duplicate method with @ResponseBody annotation
-//    @GetMapping("/showFormForUpdate/{id}/api")
-//    @ResponseBody
-//    public String showFormForUpdateRestAPI(@PathVariable( value = "id") long id, Model model) {
-//
-//        // get course from the service
-//        Course course = courseService.getCourseById(id);
-//
-//        // set course as a model attribute to pre-populate the form
-//        model.addAttribute(  "course", course);
-//        return "update_course";
-//    }
+    // Rest controller for showFormForUpdate method -- duplicate method with @ResponseBody annotation
+    @GetMapping("/showFormForUpdate/{id}/api")
+    @ResponseBody
+    public String showFormForUpdateRestAPI(@PathVariable( value = "id") long id, Model model) {
+
+        // get course from the service
+        Course course = courseService.getCourseById(id);
+
+        // set course as a model attribute to pre-populate the form
+        model.addAttribute(  "course", course);
+        return "update_course";
+    }
 
     @GetMapping("/deleteCourse/{id}")
     public String deleteCourse(@PathVariable (value = "id") long id) {
@@ -84,13 +84,13 @@ public class CourseController {
         return "redirect:/";
     }
 
-//    // Rest controller for deleteCourse method -- duplicate method with @ResponseBody annotation
-//    @GetMapping("/deleteCourse/{id}/api")
-//    @ResponseBody
-//    public String deleteCourseRestAPI(@PathVariable (value = "id") long id) {
-//
-//        // call delete course method
-//        this.courseService.deleteCourseById(id);
-//        return "redirect:/";
-//    }
+    // Rest controller for deleteCourse method -- duplicate method with @ResponseBody annotation
+    @GetMapping("/deleteCourse/{id}/api")
+    @ResponseBody
+    public String deleteCourseRestAPI(@PathVariable (value = "id") long id) {
+
+        // call delete course method
+        this.courseService.deleteCourseById(id);
+        return "redirect:/";
+    }
 }

b84d85529657e05bcfa1848c5627d3fbac55a330
rajiniw95 <rajiniw95@gmail.com>
2021-09-14 09:34:58 -0500

    add REST methods to controller with @ResponseBody annotation

--- courses/src/main/java/com/coursemgmt/courses/controller/CourseController.java
+++ courses/src/main/java/com/coursemgmt/courses/controller/CourseController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class CourseController {
 
     @Autowired
@@ -16,6 +16,14 @@ public class CourseController {
     // display list of courses
     @GetMapping("/")
     public String viewHomePage(Model model){
+        model.addAttribute("listCourses", courseService.getAllCourses());
+        return "index";
+    }
+
+    // Rest controller for viewHomePage method -- duplicate method with @ResponseBody annotation
+    @GetMapping("/api")
+    @ResponseBody
+    public String viewHomePageRestAPI(Model model){
         model.addAttribute("listCourses", courseService.getAllCourses());
         return "index";
     }
@@ -35,6 +43,15 @@ public class CourseController {
         return "redirect:/";
     }
 
+    // Rest controller for showNewCourseForm method -- duplicate method with @ResponseBody annotation
+    @PostMapping("/saveCourse/api")
+    @ResponseBody
+    public String saveCourse(@ModelAttribute("course") Course course){
+        // save course to database
+        courseService.saveCourse(course);
+        return "redirect:/";
+    }
+
     @GetMapping("/showFormForUpdate/{id}")
     public String showFormForUpdate(@PathVariable( value = "id") long id, Model model) {
 
@@ -46,7 +63,30 @@ public class CourseController {
         return "update_course";
     }
 
+    // Rest controller for showFormForUpdate method -- duplicate method with @ResponseBody annotation
+    @GetMapping("/showFormForUpdate/{id}/api")
+    @ResponseBody
+    public String showFormForUpdate(@PathVariable( value = "id") long id, Model model) {
+
+        // get course from the service
+        Course course = courseService.getCourseById(id);
+
+        // set course as a model attribute to pre-populate the form
+        model.addAttribute(  "course", course);
+        return "update_course";
+    }
+
     @GetMapping("/deleteCourse/{id}")
+    public String deleteCourse(@PathVariable (value = "id") long id) {
+
+        // call delete course method
+        this.courseService.deleteCourseById(id);
+        return "redirect:/";
+    }
+
+    // Rest controller for deleteCourse method -- duplicate method with @ResponseBody annotation
+    @GetMapping("/deleteCourse/{id}/api")
+    @ResponseBody
     public String deleteCourse(@PathVariable (value = "id") long id) {
 
         // call delete course method

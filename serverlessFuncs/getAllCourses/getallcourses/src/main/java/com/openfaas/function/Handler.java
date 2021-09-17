50e3afb57015c90c318349b6957eba0176a959df
rajiniw95 <rajiniw95@gmail.com>
2021-09-17 16:46:22 -0500

    add SQL code for getAllCourses

--- serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
+++ serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
@@ -5,19 +5,53 @@ import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 
-//import com.coursemgmt.courses.model.Course;
-//import com.coursemgmt.courses.repository.CourseRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
+import java.sql.*;
 
-//import com.coursemgmt.courses.service.CourseServiceImpl;
+import java.util.*;
 
 public class Handler extends com.openfaas.model.AbstractHandler {
 
-    public IResponse Handle(IRequest req) {
-        Response res = new Response();
-	    res.setBody("Handler for get all courses");
+    public IResponse Handle(IRequest req){
 
-	    return res;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/coursedb?useSSL=false";
+        String username = "root";
+        String password = "rajiniw95";
+        
+        Connection conn = null;
+
+        try{
+            conn = DriverManager.getConnection(url,username,password);
+            
+            String query = "SELECT * FROM courses";
+
+	Statement st = conn.createStatement();
+
+	ResultSet rs = st.executeQuery(query);
+	
+	ResultSetMetaData rsmd = rs.getMetaData();
+       //getting the column type
+       int column_count = rsmd.getColumnCount();
+       String column_count_str = String.valueOf(column_count);
+	/*
+	List allRows = new ArrayList();
+    	while(rs.next()){
+        	String[] currentRow = new String[numberColumns];
+        	for(int i = 1;i<=numberColumns;i++){
+            	row[i-1]=rs.getString(i);
+        	}
+        	rows.add(row);
+    	}
+*/
+	Response res = new Response();
+	res.setBody(column_count_str);
+
+	 return res;
+            
+        } catch(Exception e){Response res = new Response();
+         res.setBody("SQL error");
+
+	 return res;}
+   
     }
 }

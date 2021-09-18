4dab8cc070723992aa835bf6e020e34167d2f15c
rajiniw95 <rajiniw95@gmail.com>
2021-09-17 19:59:58 -0500

    fix SQL error: JDBC driver not found

--- serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
+++ serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
@@ -11,28 +11,28 @@ import java.util.*;
 
 public class Handler extends com.openfaas.model.AbstractHandler {
 
-    public IResponse Handle(IRequest req){
+    public IResponse Handle(IRequest req) {
 
         String driver = "com.mysql.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/coursedb?useSSL=false";
         String username = "root";
         String password = "rajiniw95";
-        
+
         Connection conn = null;
 
-        try{
-            conn = DriverManager.getConnection(url,username,password);
-            
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+
             String query = "SELECT * FROM courses";
 
-	Statement st = conn.createStatement();
+            Statement st = conn.createStatement();
 
-	ResultSet rs = st.executeQuery(query);
-	
-	ResultSetMetaData rsmd = rs.getMetaData();
-       //getting the column type
-       int column_count = rsmd.getColumnCount();
-       String column_count_str = String.valueOf(column_count);
+            ResultSet rs = st.executeQuery(query);
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+            //getting the column type
+            int column_count = rsmd.getColumnCount();
+            String column_count_str = String.valueOf(column_count);
 	/*
 	List allRows = new ArrayList();
     	while(rs.next()){
@@ -43,15 +43,17 @@ public class Handler extends com.openfaas.model.AbstractHandler {
         	rows.add(row);
     	}
 */
-	Response res = new Response();
-	res.setBody(column_count_str);
+            Response res = new Response();
+            res.setBody(column_count_str);
 
-	 return res;
-            
-        } catch(Exception e){Response res = new Response();
-         res.setBody("SQL error");
+            return res;
 
-	 return res;}
-   
+        } catch (Exception e) {
+            Response res = new Response();
+            res.setBody(e.toString());
+
+            return res;
+        }
+
     }
 }

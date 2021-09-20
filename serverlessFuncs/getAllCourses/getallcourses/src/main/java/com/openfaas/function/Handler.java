3e44b5977bd77ca42406b702a8a2d415cf18d342
rajiniw95 <rajiniw95@gmail.com>
2021-09-20 16:27:32 -0500

    create JDBC connection within a constructor

--- serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
+++ serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
@@ -11,42 +11,49 @@ import java.util.*;
 
 public class Handler extends com.openfaas.model.AbstractHandler {
 
+    Connection connection = null;
+
+    public void Handler() {
+        //Class.forName("com.mysql.jdbc.Driver");
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/coursedb", "root", "rajiniw95");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     public IResponse Handle(IRequest req) {
 
-        Connection connection = null;
-
         try {
-            Class.forName("com.mysql.jdbc.Driver");
 
-            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/coursedb", "root", "rajiniw95");
+            Handler handler = new Handler();
 
             Statement statement = connection.createStatement();
 
             ResultSet rs = statement.executeQuery("select * from courses");
 
             ResultSetMetaData rsmd = rs.getMetaData();
-            int column_count = rsmd.getColumnCount();
 
-            String column_count_str = String.valueOf(column_count);
-	/*
-	List allRows = new ArrayList();
-    	while(rs.next()){
-        	String[] currentRow = new String[numberColumns];
-        	for(int i = 1;i<=numberColumns;i++){
-            	row[i-1]=rs.getString(i);
-        	}
-        	rows.add(row);
-    	}
-*/
+            int column_count = rsmd.getColumnCount();
+            String column_count_string = Integer.toString(column_count);
+
+//	        List allRows = new ArrayList();
+//    	    while(rs.next()){
+//        	    String[] currentRow = new String[column_count];
+//        	    for(int i = 1;i<=column_count;i++){
+//            	    row[i-1]=rs.getString(i);
+//        	    }
+//        	    rows.add(row);
+//    	    }
+
             Response res = new Response();
-            res.setBody(column_count_str);
+            res.setBody(column_count_string);
 
             return res;
 
         } catch (Exception e) {
             Response res = new Response();
             res.setBody(e.toString());
-
             return res;
         }
 

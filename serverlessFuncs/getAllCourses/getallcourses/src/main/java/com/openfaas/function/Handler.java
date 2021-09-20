34467184d519502dc60888de20716c36f8735555
rajiniw95 <rajiniw95@gmail.com>
2021-09-20 16:49:09 -0500

    convert result set to array

--- serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
+++ serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
@@ -32,22 +32,20 @@ public class Handler extends com.openfaas.model.AbstractHandler {
 
             ResultSet rs = statement.executeQuery("select * from courses");
 
-            ResultSetMetaData rsmd = rs.getMetaData();
+//            ResultSetMetaData rsmd = rs.getMetaData();
+//
+//            int column_count = rsmd.getColumnCount();
+//            String column_count_string = Integer.toString(column_count);
 
-            int column_count = rsmd.getColumnCount();
-            String column_count_string = Integer.toString(column_count);
-
-//	        List allRows = new ArrayList();
-//    	    while(rs.next()){
-//        	    String[] currentRow = new String[column_count];
-//        	    for(int i = 1;i<=column_count;i++){
-//            	    row[i-1]=rs.getString(i);
-//        	    }
-//        	    rows.add(row);
-//    	    }
+            int i = 0;
+            String[] arrCourseName = null;
+            while(rs.next()) {
+                arrCourseName[i] = rs.getString("course_name");
+                i++;
+            }
 
             Response res = new Response();
-            res.setBody(column_count_string);
+            res.setBody(arrCourseName.toString());
 
             return res;
 

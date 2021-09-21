53f1fd68e58a4bfc5e6bb64b007217d700d821d2
rajiniw95 <rajiniw95@gmail.com>
2021-09-21 17:57:48 -0500

    changes

--- serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
+++ serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
@@ -5,36 +5,57 @@ import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 
-import com.mysql.jdbc.*;
+//import com.mysql.jdbc.*;
+
 import java.sql.*;
 import java.util.*;
 
 public class Handler extends com.openfaas.model.AbstractHandler {
 
-    Connection connection = null;
+    static Connection connection;
 
-    public void Handler() {
+    public Handler() {
         try {
             connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/coursedb", "root", "rajiniw95");
-        } catch (Exception e){
+            //System.out.println("Connected");
+        } catch (Exception e) {
             e.printStackTrace();
+            //System.out.println("NOT Connected");
         }
     }
 
     public IResponse Handle(IRequest req) {
 
         try {
-            Statement statement = connection.createStatement();/*
+            Handler handler = new Handler();
+            //System.out.println("MAIN");
 
-            ResultSet rs = statement.executeQuery("select * from courses");
+            Statement statement = connection.createStatement();
+            ResultSet resultset = statement.executeQuery("select * from courses");
 
-            ResultSetMetaData rsmd = rs.getMetaData();
+            // print all data in table
+            ArrayList<String> arrayList = new ArrayList<String>();
+            while (resultset.next()) {
+                int i = 1;
+                while (i <= 4) {
+                    arrayList.add(resultset.getString(i++));
+                }
+                //System.out.println(resultset.getString("course_code"));
+                //System.out.println(resultset.getString("course_name"));
+                //System.out.println(resultset.getString("lecturer"));
+                //System.out.println(resultset.getString("credits"));
+            }
+
+            // print number of columns
+            ResultSetMetaData rsmd = resultset.getMetaData();
 
             int column_count = rsmd.getColumnCount();
 
             Integer column_count_int = new Integer(column_count);
 
-            String column_count_string = column_count_int.toString();*/
+            String column_count_string = column_count_int.toString();
+
+            //System.out.println(column_count_string);
 
             Response res = new Response();
             res.setBody("okay");

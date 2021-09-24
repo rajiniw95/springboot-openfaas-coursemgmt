51ea2bbd88fbec2bb6b3eb3a840470cf04256d6f
rajiniw95 <rajiniw95@gmail.com>
2021-09-24 08:41:15 -0500

    remove print statements

--- serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
+++ serverlessFuncs/getAllCourses/getallcourses/src/main/java/com/openfaas/function/Handler.java
@@ -17,10 +17,8 @@ public class Handler extends com.openfaas.model.AbstractHandler {
     public Handler() {
         try {
             connection = DriverManager.getConnection("jdbc:mysql://mysql:3306/coursedb", "root", "rajiniw95");
-            //System.out.println("Connected");
         } catch (Exception e) {
             e.printStackTrace();
-            //System.out.println("NOT Connected");
         }
     }
 
@@ -28,25 +26,20 @@ public class Handler extends com.openfaas.model.AbstractHandler {
 
         try {
             //Handler handler = new Handler();
-            //System.out.println("MAIN");
 
             Statement statement = connection.createStatement();
             ResultSet resultset = statement.executeQuery("select * from courses");
 
-            // print all data in table
+            // get all data in table
             ArrayList<String> arrayList = new ArrayList<String>();
             while (resultset.next()) {
                 int i = 1;
                 while (i <= 4) {
                     arrayList.add(resultset.getString(i++));
                 }
-                //System.out.println(resultset.getString("course_code"));
-                //System.out.println(resultset.getString("course_name"));
-                //System.out.println(resultset.getString("lecturer"));
-                //System.out.println(resultset.getString("credits"));
             }
 
-            // print number of columns
+            // get number of columns
             ResultSetMetaData rsmd = resultset.getMetaData();
 
             int column_count = rsmd.getColumnCount();
@@ -54,8 +47,6 @@ public class Handler extends com.openfaas.model.AbstractHandler {
             Integer column_count_int = new Integer(column_count);
 
             String column_count_string = column_count_int.toString();
-
-            //System.out.println(column_count_string);
 
             Response res = new Response();
             res.setBody("okay");

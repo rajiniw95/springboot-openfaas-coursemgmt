298d24786113b2f655b5c30f63e1a4c88f4b77ec
rajiniw95 <rajiniw95@gmail.com>
2021-09-27 17:35:25 -0500

    add code for saveCourse serverlessFunc

--- serverlessFuncs/saveCourse/savecourse/src/main/java/com/openfaas/function/Handler.java
+++ serverlessFuncs/saveCourse/savecourse/src/main/java/com/openfaas/function/Handler.java
@@ -22,8 +22,13 @@ public class Handler extends com.openfaas.model.AbstractHandler {
 
     public IResponse Handle(IRequest req) {
         try {
-            String cid = req.getBody(); 
-            String q = "INSERT INTO courses (course_code, course_name, lecturer, credits) VALUES ('CS1005', 'Research Methods', 'Stacy Peters', '100')";
+            String req_string = req.getBody(); 
+            String[] req_array = req_string.split("\\s*,\\s*");
+            String course_code = req_array[0];
+            String course_name = req_array[1];
+            String lecturer = req_array[2];
+            String credits = req_array[3];
+            String q = "INSERT INTO courses (course_code, course_name, lecturer, credits) VALUES ('" + course_code + "', '" + course_name + "', '" + lecturer + "', '" + credits + "')";
             
             Statement statement = connection.createStatement();
             int executed = statement.executeUpdate(q);

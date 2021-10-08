621983c495a7db67d1413465e1afdb9cd63cccca
rajiniw95 <rajiniw95@gmail.com>
2021-10-08 11:46:48 -0500

    process http response for get all courses

--- courses/src/main/java/com/coursemgmt/courses/controller/CourseController.java
+++ courses/src/main/java/com/coursemgmt/courses/controller/CourseController.java
@@ -22,6 +22,9 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
 
+import org.json.simple.*;
+import com.google.gson.*;
+
 @Controller
 public class CourseController {
 
@@ -35,8 +38,8 @@ public class CourseController {
     // display list of courses
     @GetMapping("/")
     public String viewHomePage(Model model) {
-    
-        /*
+
+
         String uri = "http://127.0.0.1:31112/function/getallcourses";
 
         try {
@@ -45,58 +48,73 @@ public class CourseController {
                     .uri(URI.create(uri))
                     .build();
 
-	    // get HTTP response
+            // get HTTP response
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-            
+
             // extract body of HTTP response
             String response_body = response.body();
             System.out.println("BEGIN RESPONSE BODY FOR GET ALL COURSES");
             response_body = response_body.replaceAll(", $", "");
             System.out.println(response_body);
             System.out.println("END RESPONSE BODY FOR GET ALL COURSES");
-            
+
+            // split response body by commas and assign to array
             String[] array_response = response_body.split(",");
-            
+
+            // get array size
             int array_size = array_response.length;
-                        
+
+            // remove extra spaces from all array elements
             String[] trimmed_array = new String[array_size];
-	    for (int i = 0; i < array_response.length; i++)
-		trimmed_array[i] = array_response[i].trim();
-            
+            for (int i = 0; i < array_response.length; i++)
+                trimmed_array[i] = array_response[i].trim();
+
+            // define number of attributes in class
             int class_size = 5;
-            
+
+            // define list of course objects, to add the gson objects to and return to model
+            List<Course> course_obj_list = new ArrayList<Course>();
+
             int n = 1;
-            
+
+            // loop to break response array in to class_size chunks, so that each can be converted to an object
             for (int i = 0; i < array_size; i = i + class_size) {
-       	     // String array_name = "array" + n; 
-       	     // System.out.println(array_name);
-       	     List<String> list = new ArrayList<String>();
-       	     int k = i;
-       	     for (int j = 0; j < class_size; j++){
-       	     	list.add(trimmed_array[k]);
-       	     	k++;
-       	     }
-       	     
-       	     System.out.println(Arrays.toString(list.toArray()));
-       	     n++;
+                // String array_name = "array" + n;
+                // System.out.println(array_name);
+
+                // add object elements to arraylist
+                List<String> list = new ArrayList<String>();
+                int k = i;
+                for (int j = 0; j < class_size; j++) {
+                    list.add(trimmed_array[k]);
+                    k++;
+                }
+
+                System.out.println(Arrays.toString(list.toArray()));
+                n++;
+
+                // define array list as json string
+                String json_str = "{\"id\":\"" + list.get(0) + "\", \"courseCode\":\"" + list.get(1) + "\", \"courseName\":\"" + list.get(2) + "\", \"lecturer\":\"" + list.get(3) + "\", \"credits\":\"" + list.get(4) + "\"}";
+                System.out.println(json_str);
+
+                // Creating a Gson Object
+                Gson gson = new Gson();
+
+                // convert json string to Course object
+                Course course_gson = gson.fromJson(json_str, Course.class);
+
+                // add new course object to course object list
+                course_obj_list.add(course_gson);
+
             }
 
+            // add course object list to model
+            model.addAttribute("listCourses", course_obj_list);
             return "index";
         } catch (Exception e) {
             return e.toString();
         }
-        */
-    
-        model.addAttribute("listCourses", courseService.getAllCourses());
-	return "index";
     }
-
-//    // REST API to view JSON of home page
-//    @GetMapping("/api")
-//    @ResponseBody
-//    public List<Course> viewHomePageRestAPI() {
-//        return courseService.getAllCourses();
-//    }
 
     //============================ DISPLAY PAGE TO ADD NEW COURSE ============================//
 
@@ -112,37 +130,34 @@ public class CourseController {
 
     @PostMapping("/saveCourse")
     public String saveCourse(@ModelAttribute("course") Course course) {
-        
+
         // assign object attributes to variables 
         String input_course_code = course.getCourseCode();
         String input_course_name = course.getCourseName();
         String input_lecturer = course.getLecturer();
         int input_credits = course.getCredits();
-        String string_input_credits =String.valueOf(input_credits);
-        
+        String string_input_credits = String.valueOf(input_credits);
+
         // construct parameter for http request with object attributes
-        String uri_parameter = input_course_code + ","+ input_course_name + ","+ input_lecturer + ","+ string_input_credits;
+        String uri_parameter = input_course_code + "," + input_course_name + "," + input_lecturer + "," + string_input_credits;
         // replace all spaces in uri with %20
         uri_parameter = uri_parameter.replaceAll(" ", "%20");
-        
+
         System.out.println("UPDATING DATABASE WITH COURSE...");
-	System.out.println(uri_parameter);
-	
-	// set uri to savecourse serverless function with paramemeter uri_parameter
-	String uri = "http://127.0.0.1:31112/function/savecourse?data=" + uri_parameter;
-	
-	try {
+        System.out.println(uri_parameter);
+
+        // set uri to savecourse serverless function with paramemeter uri_parameter
+        String uri = "http://127.0.0.1:31112/function/savecourse?data=" + uri_parameter;
+
+        try {
             // send HTTP request
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(uri))
                     .build();
 
-	    // get HTTP response
+            // get HTTP response
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-                 
-            // save course to database
-            // courseService.saveCourse(course);
-            
+
             // redirect to home page 
             return "redirect:/";
         } catch (Exception e) {
@@ -151,26 +166,19 @@ public class CourseController {
 
     }
 
-//    // REST API to save a course
-//    @PostMapping(value = "/saveCourse/api", consumes = "application/json", produces = "application/json")
-//    @ResponseBody
-//    public Course saveCourseRestAPI(@Valid @RequestBody Course course) {
-//        return courseService.saveCourse(course);
-//    }
-
     //============================ VIEW UPDATE FORM ============================//
 
     @GetMapping("/showFormForUpdate/{id}")
     public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
 
-	// check course ID
-	long cid = id;
-	System.out.println("PRINT ID FOR GET COURSE BY ID...");
-	System.out.println(cid);
-	System.out.println("END ID FOR GET COURSE BY ID");
-	
-	// set uri to getcoursebyid serverless function with paramemeter cid
-	String uri = "http://127.0.0.1:31112/function/getcoursebyid?cid=" + cid;
+        // check course ID
+        long cid = id;
+        System.out.println("PRINT ID FOR GET COURSE BY ID...");
+        System.out.println(cid);
+        System.out.println("END ID FOR GET COURSE BY ID");
+
+        // set uri to getcoursebyid serverless function with paramemeter cid
+        String uri = "http://127.0.0.1:31112/function/getcoursebyid?cid=" + cid;
 
         try {
             // send HTTP request
@@ -178,16 +186,16 @@ public class CourseController {
                     .uri(URI.create(uri))
                     .build();
 
-	    // get HTTP response
+            // get HTTP response
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-            
+
             // extract body of HTTP response
             String response_body = response.body();
             System.out.println("BEGIN RESPONSE BODY FOR GET COURSE BY ID");
             response_body = response_body.replaceAll(", $", "");
             System.out.println(response_body);
             System.out.println("END RESPONSE BODY FOR GET COURSE BY ID");
-            
+
             // put response body to array and assign elements to variables
             String[] course_detail_list = response_body.split(",");
             String response_course_code = course_detail_list[0];
@@ -195,15 +203,15 @@ public class CourseController {
             String response_lecturer = course_detail_list[2];
             String response_credits = course_detail_list[3];
             String trimmed_response_creedits = response_credits.trim();
-            int int_credits=Integer.parseInt(trimmed_response_creedits);  
-                   
+            int int_credits = Integer.parseInt(trimmed_response_creedits);
+
             // create course object with response variables 
             Course course = new Course();
             course.setCourseCode(response_course_code);
             course.setCourseName(response_course_name);
             course.setLecturer(response_lecturer);
             course.setCredits(int_credits);
-            
+
             //Course course = courseService.getCourseById(id);
 
             // set course as a model attribute to pre-populate the form
@@ -215,25 +223,18 @@ public class CourseController {
         }
     }
 
-//    // REST API to view update course form
-//    @GetMapping("/showFormForUpdate/{id}/api")
-//    @ResponseBody
-//    public Course showFormForUpdateRestAPI(@PathVariable(value = "id") long id) {
-//        return courseService.getCourseById(id);
-//    }
-
     //============================ DELETE COURSE ============================//
 
     @GetMapping("/deleteCourse/{id}")
     public String deleteCourse(@PathVariable(value = "id") long id) {
-    
+
         // check course ID
-    	System.out.println("PRINT ID FOR DELETE COURSE BY ID...");
-	System.out.println(id);
-	System.out.println("END ID FOR DELETE COURSE BY ID");
-	
-	// set uri to deletecoursebyid serverless function with paramemeter cid
-	String uri = "http://127.0.0.1:31112/function/deletecoursebyid?cid=" + id;
+        System.out.println("PRINT ID FOR DELETE COURSE BY ID...");
+        System.out.println(id);
+        System.out.println("END ID FOR DELETE COURSE BY ID");
+
+        // set uri to deletecoursebyid serverless function with paramemeter cid
+        String uri = "http://127.0.0.1:31112/function/deletecoursebyid?cid=" + id;
 
         try {
             // send HTTP request
@@ -241,23 +242,16 @@ public class CourseController {
                     .uri(URI.create(uri))
                     .build();
 
-	    // get HTTP response
+            // get HTTP response
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
             // call delete course method
             //this.courseService.deleteCourseById(id);
-            
+
             // redirect to home page 
             return "redirect:/";
         } catch (Exception e) {
             return e.toString();
         }
     }
-
-//    // REST API to delete course
-//    @GetMapping("/deleteCourse/{id}/api")
-//    @ResponseBody
-//    public long deleteCourseRestAPI(@PathVariable(value = "id") long id) {
-//        return courseService.deleteCourseById(id);
-//    }
 }

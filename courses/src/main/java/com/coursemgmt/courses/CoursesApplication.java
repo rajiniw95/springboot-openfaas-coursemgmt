a831d25a763c67104ce453be552eece0ba6a4048
rajiniw95 <rajiniw95@gmail.com>
2021-12-02 17:18:07 -0600

    JAVA invoke HTTP requests for GET requests defined in controller

--- courses/src/main/java/com/coursemgmt/courses/CoursesApplication.java
+++ courses/src/main/java/com/coursemgmt/courses/CoursesApplication.java
@@ -12,74 +12,15 @@ public class CoursesApplication {
 		SpringApplication.run(CoursesApplication.class, args);
 		
 		HTTPRequestGenerator httpreq = new HTTPRequestGenerator();
-		httpreq.helloooo();
 		
 		try {
-            		System.out.println("Testing 1 - Send Http GET request");
             		httpreq.sendGET_home();
-
-            		System.out.println("Testing 2 - Send Http POST request");
-            		httpreq.sendPOST_save_course();
+            		httpreq.sendGET_update_form("909");
+            		// httpreq.sendGET_new_course();
+            		// httpreq.sendPOST_save_course();
+            		// httpreq.sendGET_delete_course("916");
         	} finally {
             		httpreq.close();
         	}
-		/*System.out.println("Hello World!!!");
-		
-		CloseableHttpClient httpClient = HttpClients.createDefault();
-		
-		try {
-            		HttpGet request = new HttpGet("http://127.0.0.1:8080/");
-
-            		// add request headers
-            		request.addHeader("custom-key", "mkyong");
-            		request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
-
-            		CloseableHttpResponse response = httpClient.execute(request);
-
-            		try {
-                		// Get HttpResponse Status
-                		System.out.println(response.getProtocolVersion());              // HTTP/1.1
-                		System.out.println(response.getStatusLine().getStatusCode());   // 200
-                		System.out.println(response.getStatusLine().getReasonPhrase()); // OK
-                		System.out.println(response.getStatusLine().toString());        // HTTP/1.1 200 OK
-
-                		HttpEntity entity = response.getEntity();
-                		if (entity != null) {
-                    			// return it as a String
-                    			String result = EntityUtils.toString(entity);
-                    			System.out.println(result);
-                		}
-                		System.out.println("POST SENT");
-            		} finally {
-                		response.close();
-            		}
-        	} finally {
-            		httpClient.close();
-        	}*/
     	}
-    	/*
-    	private void sendGet() throws Exception {
-
-        	HttpGet request = new HttpGet("http://127.0.0.1:8080/");
-
-        	// add request headers
-        	request.addHeader("custom-key", "mkyong");
-        	request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
-
-        	try (CloseableHttpResponse response = httpClient.execute(request)) {
-	
-            		// Get HttpResponse Status
-            		System.out.println(response.getStatusLine().toString());
-
-            		HttpEntity entity = response.getEntity();
-            		Header headers = entity.getContentType();
-            		System.out.println(headers);
-
-            		if (entity != null) {
-                		// return it as a String
-                		String result = EntityUtils.toString(entity);
-                		System.out.println(result);
-            		}
-        	}
-    	}*/
 }
--- courses/src/main/java/com/coursemgmt/courses/monitoring/HTTPRequestGenerator.java
+++ courses/src/main/java/com/coursemgmt/courses/monitoring/HTTPRequestGenerator.java
@@ -28,23 +28,20 @@ import java.util.List;
 public class HTTPRequestGenerator {
 
 	// declare variables for URIs
-	private static String uri_home = "http://127.0.0.1:8080/"
-	private static String uri_new_course = "http://127.0.0.1:8080/showNewCourseForm"
-	private static String uri_update_form = "http://127.0.0.1:8080/showFormForUpdate/"
-	private static String uri_save_course = "http://127.0.0.1:8080/saveCourse"
-	private static String uri_update_course = "http://127.0.0.1:8080/updateCourse"
-	private static String uri_delete_course = "http://127.0.0.1:8080/deleteCourse/"
-	
-	private final CloseableHttpClient http_client = HttpClients.createDefault();
+	private static String uri_home = "http://127.0.0.1:8080/";
+	private static String uri_new_course = "http://127.0.0.1:8080/showNewCourseForm/";
+	private static String uri_update_form = "http://127.0.0.1:8080/showFormForUpdate/";
+	private static String uri_save_course = "http://127.0.0.1:8080/saveCourse";
+	private static String uri_update_course = "http://127.0.0.1:8080/updateCourse";
+	private static String uri_delete_course = "http://127.0.0.1:8080/deleteCourse/";
 
-    	public static void helloooo() {
-    		System.out.println("Hellooooo ");
-  	}
+	private final CloseableHttpClient http_client = HttpClients.createDefault();
   	
   	public void close() throws IOException {
         	http_client.close();
     	}
     	
+    	// http://127.0.0.1:8080/
     	public void sendGET_home() throws Exception {
 
         	HttpGet request = new HttpGet(uri_home);
@@ -53,13 +50,15 @@ public class HTTPRequestGenerator {
             		// Get HttpResponse Status
             		System.out.println(response.getStatusLine().toString());
             		HttpEntity entity = response.getEntity();
-
             		if (entity != null) {
-                		System.out.println("GET request for the HOME PAGE successfully sent");
+            			// String result = EntityUtils.toString(entity);
+                		// System.out.println(result);
+                		System.out.println("VIEW HOME PAGE request sent successfully");
             		}
         	}
 	}
 	
+	// http://127.0.0.1:8080/showNewCourseForm/
 	public void sendGET_new_course() throws Exception {
 
         	HttpGet request = new HttpGet(uri_new_course);
@@ -70,22 +69,56 @@ public class HTTPRequestGenerator {
             		HttpEntity entity = response.getEntity();
 
             		if (entity != null) {
-                		System.out.println("GET request for the NEW COURSE PAGE successfully sent");
+                  		System.out.println("NEW COURSE PAGE request sent successfully");
             		}
         	}
 	}
-	    
+	
+	// http://127.0.0.1:8080/deleteCourse/
+	public void sendGET_delete_course(String cid) throws Exception {
+
+    		String uri_with_param = uri_delete_course + cid;
+        	HttpGet request = new HttpGet(uri_with_param);        	
+        	
+        	try (CloseableHttpResponse response = http_client.execute(request)) {
+            		// Get HttpResponse Status
+            		System.out.println(response.getStatusLine().toString());
+            		HttpEntity entity = response.getEntity();
+            		if (entity != null) {
+                		System.out.println("DELETE request for course ID " + cid + " sent successfully");
+            		} 
+        	}
+    	}
+    	
+    	// http://127.0.0.1:8080/showFormForUpdate/
+    	public void sendGET_update_form(String cid) throws Exception {
+
+    		String uri_with_param = uri_update_form + cid;
+        	HttpGet request = new HttpGet(uri_with_param);        	
+        	
+        	try (CloseableHttpResponse response = http_client.execute(request)) {
+            		// Get HttpResponse Status
+            		System.out.println(response.getStatusLine().toString());
+            		HttpEntity entity = response.getEntity();
+            		if (entity != null) {
+                		System.out.println("GET UPDATE COURSE FORM request for course ID " + cid + " sent successfully");
+            		} 
+        	}
+    	}
+	
+	// incomplete   
+	// http://127.0.0.1:8080/saveCourse 
 	public void sendPOST_save_course() throws Exception {
 
         	HttpPost post = new HttpPost(uri_save_course);
 
         	// add request parameter, form parameters
         	List<NameValuePair> urlParameters = new ArrayList<>();
-        	urlParameters.add(new BasicNameValuePair("data", "CS1008,Human Computer Interaction,Sarah Sebo,10000"));
-        	urlParameters.add(new BasicNameValuePair("password", "123"));
-        	urlParameters.add(new BasicNameValuePair("custom", "secret"));
-
-//http://127.0.0.1:31112/function/savecourse?data=CS1008,Human%20Computer%20Interaction,Sarah%20Sebo,100
+        	urlParameters.add(new BasicNameValuePair("data", "CS1008,Human%20Computer%20Interaction,Sarah%20Sebo,100001"));
+        	//urlParameters.add(new BasicNameValuePair("password", "123"));
+        	//urlParameters.add(new BasicNameValuePair("custom", "secret"));
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+		//http://127.0.0.1:31112/function/savecourse?data=CS1008,Human%20Computer%20Interaction,Sarah%20Sebo,100
 
 
         	post.setEntity(new UrlEncodedFormEntity(urlParameters));
@@ -96,4 +129,6 @@ public class HTTPRequestGenerator {
         	}
 
     	}
+    	
+    	
 }

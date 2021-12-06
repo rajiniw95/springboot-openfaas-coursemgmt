411724dede3d0fc8b2fda1d1582cf26504db25ef
rajiniw95 <rajiniw95@gmail.com>
2021-12-05 20:33:15 -0600

    remove unnecessary method

--- courses/src/main/java/com/coursemgmt/courses/monitoring/HistogramGenerator.java
+++ courses/src/main/java/com/coursemgmt/courses/monitoring/HistogramGenerator.java
@@ -22,56 +22,6 @@ public class HistogramGenerator {
    	
    	static Histogram histogram = new Histogram(TimeUnit.SECONDS.toNanos(1), 3);
 	// (max recorded value, decimal point precision)             
-
-    	static public volatile DatagramSocket socket;
-
-    	static long WARMUP_TIME_MSEC = 5000;
-    	static long RUN_TIME_MSEC = 20000;
-
-
-    	static void recordTimeToCreateAndCloseDatagramSocket() {
-        	long startTime = System.nanoTime();
-        	try {
-            		socket = new DatagramSocket();
-        	} catch (SocketException ex) {
-        	} finally {
-            		socket.close();
-        	}
-        	long endTime = System.nanoTime();
-        	histogram.recordValue(endTime - startTime);
-    	}
-
-    	public static void create_histogram_test() {        
-        	long startTime = System.currentTimeMillis();
-        	long now;
-
-        	do {
-            		recordTimeToCreateAndCloseDatagramSocket();
-            		now = System.currentTimeMillis();
-        	} while (now - startTime < WARMUP_TIME_MSEC);
-
-        	histogram.reset();
-
-        	do {
-            		recordTimeToCreateAndCloseDatagramSocket();
-            		now = System.currentTimeMillis();
-        	} while (now - startTime < RUN_TIME_MSEC);
-        
-        	// define file name of output desitination
-        	String filename = "histogram_output_testing.txt";     
-        
-        	// Write Output Percentile Distribution to PrintStream
-        	try(PrintStream ps = new PrintStream(filename)){
-        		ps.println("Recorded latencies [in sec] for Create+Close of a DatagramSocket:");
-            		histogram.outputPercentileDistribution(ps, 1000.0);
-            		ps.println("UPDATE APPLIED");
-            		ps.flush();
-        	} catch (FileNotFoundException e) {
-            		e.printStackTrace();
-        	}
-        
-        	System.out.println("Self Scaling Histogram Created and Analyzed");
-    	}
     	
     	public static void create_histogram(String workload_type, String output_histogram_location, ArrayList<Long> delta_durations) throws IOException{        
         	long startTime = System.currentTimeMillis();

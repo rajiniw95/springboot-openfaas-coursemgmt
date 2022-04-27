dac0d5b43dd6874fedced9597836b662dd11ea92
rajiniw95 <rajiniw95@gmail.com>
2022-04-27 09:44:45 -0500

    migration .sh

---
+++ migration/sourcesite.sh
@@ -0,0 +1,8 @@
+mysqldump -u root -p password > dump.sql
+
+scp /home/rajini/dump.sql rajini@river-fe3.cs.uchicago.edu:/home/rajini/prototype-ps/
+
+scp /home/rajini/Desktop/coursemgmt-openfaas/serverlessFuncs rajini@river-fe3.cs.uchicago.edu:/home/rajini/prototype-ps/serverlessfuncs/
+
+
+

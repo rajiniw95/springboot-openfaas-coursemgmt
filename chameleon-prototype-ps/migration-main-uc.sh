0b82593d96c4fcb1005136b5fcb62832d26b777c
Chameleon Cloud User <cc@ra-rtablesl-source-aug09-instance-server-yae63sfuqglm.novalocal>
2022-08-09 21:15:43 +0000

    deploy funcs latency

--- chameleon-prototype-ps/migration-main-uc.sh
+++ chameleon-prototype-ps/migration-main-uc.sh
@@ -7,22 +7,30 @@ source variables.sh
 ################ MIGRATE SERVERLESS FUNCTIONS ################  
 
 ### Deploy openfaas at the destination
-#deploy_sql=`ssh cc@192.5.87.211 'bash -s' < destination_deploy_openfaas.sh` 
-#echo $deploy_sql
-#deploy_sql=${deploy_sql: -7}
-#echo $deploy_sql
+#deploy_openfaas=`ssh cc@192.5.87.211 'bash -s' < destination_deploy_openfaas.sh` 
+#echo $deploy_openfaas
+#deploy_openfaas=${deploy_openfaas: -7}
+#echo $deploy_openfaas
 #echo "Deploy OpenFaas at the destination (seconds) = " >> $latency_file_name
-#echo $deploy_sql >> $latency_file_name
+#echo $deploy_openfaas >> $latency_file_name
 
 ### Copy serverless function files from the source to the destination
-source s2t_copy_sl_files.sh 
+#source s2t_copy_sl_files.sh 
 
 ### Copy MySQL yaml for container creation from source to destination
-source s2t_copy_sql_yaml.sh
+#source s2t_copy_sql_yaml.sh
 
 ### Create the database dump at the source
-source source_generate_db_file.sh
+#source source_generate_db_file.sh
 
 ### Copy the database dump file from the source to the target
 # upload the dump file and get the link
-dump_link=$(source source_upload_db_file.sh)
+#dump_link=$(source source_upload_db_file.sh)
+
+### Deploy the serverless functions at the destination
+deploy_funcs=`ssh cc@192.5.87.211 'bash -s' < destination_deploy_funcs.sh`
+# TODO: extract the last space delimited substring
+echo "DEPLOYED!!!!"
+deploy_funcs=${deploy_funcs: -6}
+echo "Deploy serverless functions at the destination (seconds) = " >> $latency_file_name
+echo $deploy_funcs >> $latency_file_name

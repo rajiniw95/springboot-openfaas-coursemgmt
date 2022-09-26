0da1b641929301dec3b982fd69ff1054908e8762
Chameleon Cloud User <cc@ra-esl-source-tacc-sep12-instance-server-kikdlahfkcer.novalocal>
2022-09-26 21:48:09 +0000

    add comments

--- chameleon-prototype-ps/critical-path/s2t_transfer_db_files.sh
+++ chameleon-prototype-ps/critical-path/s2t_transfer_db_files.sh
@@ -1,3 +1,9 @@
+# https://justyn.io/til/copy-files-directories-from-one-kubernetes-pod-to-another-kubernetes-pod/
+
+# pipe the stdout of the tar command to the stdin of tar in a separate pod, similar to using tar to copy files between two servers without rsync/scp
+# copies the contents of /dir1 from pod-01 to the same location in pod-0
+# kubectl exec pod-01 -- tar cf - /dir1 /dir2 | kubectl exec -i pod-02 -- tar xvf - -C /
+
 start_time=$(date +%s.%6N)
 
 sudo kubectl exec $podname_source -- tar cf - /var/lib/mysql | ssh $destination_ssh sudo kubectl exec -i $podname_target -- tar xf - -C /
